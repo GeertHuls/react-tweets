6b39c7d62450daab893f66573ea4096cde5767b3
Geert Huls <geert.huls@macadam.eu>
2015-01-12 22:32:58 +0100

    Added connection to a mongodb document store

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    routes = require('./routes/index'),
+    users = require('./routes/user');
 
 var app = express();
 
-// view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.disable('etag');
+
+mongoose.connect('mongodb://localhost:28017/react-tweets');
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
