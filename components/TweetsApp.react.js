5ee1fc0a956f59d59267f6a22b5185431bd377f1
GeertHuls <geert.huls82@gmail.com>
2015-03-18 23:04:59 +0100

    Additional methods to main tweets app serving client side functionality

--- components/TweetsApp.react.js
+++ components/TweetsApp.react.js
@@ -8,6 +8,62 @@ var NotificationBar = require('./NotificationBar.react.js');
 // Export the TweetsApp component
 module.exports = TweetsApp = React.createClass({
 
+// Method to add a tweet to our timeline
+  addTweet: function(tweet){
+
+    // Get current application state
+    var updated = this.state.tweets;
+
+    // Increment the unread count
+    var count = this.state.count + 1;
+
+    // Increment the skip count
+    var skip = this.state.skip + 1;
+
+    // Add tweet to the beginning of the tweets array
+    updated.unshift(tweet);
+
+    // Set application state
+    this.setState({tweets: updated, count: count, skip: skip});
+
+  },
+
+  // Method to show the unread tweets
+  showNewTweets: function(){
+
+    // Get current application state
+    var updated = this.state.tweets;
+
+    // Mark our tweets active
+    updated.forEach(function(tweet){
+      tweet.active = true;
+    });
+
+    // Set application state (active tweets + reset unread count)
+    this.setState({tweets: updated, count: 0});
+
+  },
+
+  // Method to check if more tweets should be loaded, by scroll position
+  checkWindowScroll: function(){
+
+    // Get scroll pos & window data
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var s = document.body.scrollTop;
+    var scrolled = (h + s) > document.body.offsetHeight;
+
+    // If scrolled enough, not currently paging and not complete...
+    if(scrolled && !this.state.paging && !this.state.done) {
+
+      // Set application state (Paging, Increment page)
+      this.setState({paging: true, page: this.state.page + 1});
+
+      // Get the next page of tweets from the server
+      this.getPage(this.state.page);
+
+    }
+  },
+
   // Set the initial component state
   getInitialState: function(props){
 
@@ -38,7 +94,7 @@ module.exports = TweetsApp = React.createClass({
     socket.on('tweet', function (data) {
 
         // Add a tweet to our queue
-        //self.addTweet(data);
+        self.addTweet(data);
 
     });
 
@@ -47,6 +103,11 @@ module.exports = TweetsApp = React.createClass({
 
   },
 
+  // don't know why this is useful...
+  // componentWillReceiveProps: function(newProps, oldProps){
+  //   this.setState(this.getInitialState(newProps));
+  // },
+
   // Render the component
   render: function(){
 
@@ -54,7 +115,7 @@ module.exports = TweetsApp = React.createClass({
       <div className="tweets-app">
         <Tweets tweets={this.state.tweets} />
         <Loader paging={this.state.paging} />
-        <NotificationBar count={this.state.count} />
+        <NotificationBar count={this.state.count} onShowNewTweets={this.showNewTweets}/>
       </div>
     )
 
